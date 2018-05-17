0d48d4c0fd91c96cc9ec1a4f4966e5dcd126e9be
bobr <bob.reyniers@hotmail.com>
2018-05-17 20:55:20 +0200

    bobr: item entity en type toegevoegd

--- src/main/kotlin/com/cegeka/explorationdays/backend/item/Item.kt
+++ src/main/kotlin/com/cegeka/explorationdays/backend/item/Item.kt
@@ -1,5 +1,57 @@
 package com.cegeka.explorationdays.backend.item
 
+import com.cegeka.explorationdays.backend.beoordeling.Beoordeling
+import com.cegeka.explorationdays.backend.niveau.Niveau
+import com.cegeka.explorationdays.backend.tags.Tags
+import javax.persistence.*
+
+@Entity
+@Table(name = "Item")
 class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Int = 0
+
+    @Column(name = "naam")
+    var naam: String = ""
+
+    @Column(name = "type")
+    var type: ItemType = ItemType.ARTIKEL
+
+    @Column(name = "links")
+    var links: List<String>? = null
+
+    @Column(name = "technologie")
+    var technologiën: List<Tags>? = null
+
+    @Column(name = "niveau")
+    var niveau: Niveau = Niveau.JUNIOR
+
+    @Column(name = "beoordelingen")
+    var beoordelingen: Beoordeling? = null
+
+    @Column(name = "thumbnail")
+    var thumbnail: Byte? = null
+
+    @Column(name = "omschrijving")
+    var omschrijving: String = ""
+
+
+    constructor() {}
+
+    constructor(naam: String, type: ItemType, links: List<String>, technologie: List<Tags>, niveau: Niveau, beoordeling: Beoordeling, thumbnail: Byte, omschrijving: String) {
+        this.naam = naam
+        this.type = type
+        this.links = links
+        this.technologiën = technologie
+        this.niveau = niveau
+        this.beoordelingen = beoordeling
+        this.thumbnail = thumbnail
+        this.omschrijving = omschrijving
+    }
+
+
 
 }
---
+++ src/main/kotlin/com/cegeka/explorationdays/backend/item/ItemType.kt
@@ -0,0 +1,10 @@
+package com.cegeka.explorationdays.backend.item
+
+/**
+ * Created by bobr on 17/05/2018.
+ */
+enum class ItemType {
+    VIDEO,
+    BOEK,
+    ARTIKEL
+}
