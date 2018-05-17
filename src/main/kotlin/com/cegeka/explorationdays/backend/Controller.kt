0c9e08f4c30e55d79d150403aba1663225c262b1
XanVranck <vranckaert.xan@gmail.com>
2018-05-17 15:07:06 +0200

    backend EDULINX met travis

---
+++ src/main/kotlin/com/cegeka/explorationdays/backend/Controller.kt
@@ -0,0 +1,16 @@
+package com.cegeka.explorationdays.backend
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+
+
+    @GetMapping("/exploration-days-backend")
+    @CrossOrigin("http://localhost:4000")
+    fun helloworld(): String {return "hello world"}
+
+
+}
