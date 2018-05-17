b53fdc169eee65f9b21d35238de830af92d8a28c
bobr <bob.reyniers@hotmail.com>
2018-05-17 20:42:05 +0200

    bobr: niveau enum toegevoegd

---
+++ src/main/kotlin/com/cegeka/explorationdays/backend/niveau/Niveau.kt
@@ -0,0 +1,13 @@
+package com.cegeka.explorationdays.backend.niveau
+
+/**
+ * Created by bobr on 17/05/2018.
+ */
+enum class Niveau {
+
+    JUNIOR,
+    MEDIOR,
+    SENIOR,
+    WIZARD,
+    WIM
+}
